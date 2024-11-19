67cdd1f64807f2de9d0bdaaed4764bc5e54b17fa
Raja Prasad <113759499+merajaprasad@users.noreply.github.com>
2024-11-19 16:43:53 +0530

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -12,4 +12,18 @@ sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.0/manifests/calico.yaml
 
 # run below command to get the token
-kubeadm token create --print-join-command
+NEW_JOIN_TOKEN=$(kubeadm token create --print-join-command)
+
+# Print the join token for verification
+echo "New Join Token Command:"
+echo "$NEW_JOIN_TOKEN"
+
+# Extract the 'kubeadm join' line and store it in a variable
+OLD_JOIN_COMMAND=$(grep "^kubeadm join" node-join.sh)
+echo "Old Join Command:"
+echo "$OLD_JOIN_COMMAND"
+
+# Replced with New token
+sed -i "s/$OLD_JOIN_COMMAND/$NEW_JOIN_TOKEN --v=5/g" join-node.sh
+
+echo "The join token has been updated successfully in node-join.sh"
